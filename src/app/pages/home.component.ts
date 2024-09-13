efeeb54ee83c39b99f4c6aea3f92be0144024613
owtf086 <owtf086@gmail.com>
2024-09-13 13:44:03 +0300

    test env

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { environment } from "../../environments/environment";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private apiUrl = environment.apiUrl;
+  private apiVal = environment.production;
 
+  ngOnInit(): void {
+    console.log('Hello World!', this.apiUrl, this.apiVal);
+  }
 }
