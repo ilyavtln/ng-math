895643b5787533bfd40cf329f2601ad188e75b3b
owtf086 <owtf086@gmail.com>
2024-09-13 12:15:06 +0300

    added env

--- src/app/directives/formula/formula.directive.ts
+++ src/app/directives/formula/formula.directive.ts
@@ -9,10 +9,14 @@ export class FormulaDirective implements AfterViewInit {
   constructor(private _el: ElementRef, private _renderer: Renderer2) {}
 
   ngAfterViewInit(): void {
+    this.inputText = this.inputText.replaceAll('<<', '{{')
 
     this._renderer.setProperty(this._el.nativeElement, 'innerHTML', this.inputText);
 
     console.log(this.inputText);
+
+    // Или, если нужно применить форматирование:
+    // this.renderer.setProperty(this.el.nativeElement, 'innerHTML', this.inputText);
   }
 
 }
