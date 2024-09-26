c0d383ca8b8dc52ed663c64b3350bd97e4a7c9ef
ilyavtln <owtf086@gmail.com>
2024-09-26 08:43:26 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/ilyavtln/ng-math/commit/50e9cde59c53a7345c28f461936806d6fef4a97d

--- chunk-6GLY3P6R.js
+++
@@ -1,1 +0,0 @@
-import{ha as r,p as t,u as i}from"./chunk-I7WMEMYV.js";var e=class o{static \u0275fac=function(m){return new(m||o)};static \u0275mod=i({type:o});static \u0275inj=t({imports:[r]})};export{e as a};
---
+++ chunk-W7M7ISDD.js
@@ -0,0 +1,3 @@
+import{H as r,I as c,P as n,aa as p,la as f,o as s,s as a,t as m}from"./chunk-4IYA573K.js";var o=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=a({type:e,selectors:[["math-basic"]],decls:3,vars:0,template:function(t,C){t&1&&(r(0,"p"),n(1,"basic works!"),c(),n(2,`
+\u041E\u0441\u043D\u043E\u0432\u044B
+`))}})};var d=[{path:"",component:o}],u=class e{static \u0275fac=function(t){return new(t||e)};static \u0275mod=m({type:e});static \u0275inj=s({imports:[p,f.forChild(d)]})};export{u as BasicModule};
