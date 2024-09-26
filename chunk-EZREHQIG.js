ea15a60dc0562fb62857f6073038e700bd6e7bc6
ilyavtln <owtf086@gmail.com>
2024-09-26 07:50:23 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/ilyavtln/ng-math/commit/9e257c65375c61441be9e65d2daa9a72ff954dd3

---
+++ chunk-EZREHQIG.js
@@ -0,0 +1,3 @@
+import{L as r,M as c,T as n,ha as p,p as s,sa as f,t as a,u as m}from"./chunk-I7WMEMYV.js";var o=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=a({type:e,selectors:[["math-basic"]],decls:3,vars:0,template:function(t,C){t&1&&(r(0,"p"),n(1,"basic works!"),c(),n(2,`
+\u041E\u0441\u043D\u043E\u0432\u044B
+`))}})};var d=[{path:"",component:o}],u=class e{static \u0275fac=function(t){return new(t||e)};static \u0275mod=m({type:e});static \u0275inj=s({imports:[p,f.forChild(d)]})};export{u as BasicModule};
